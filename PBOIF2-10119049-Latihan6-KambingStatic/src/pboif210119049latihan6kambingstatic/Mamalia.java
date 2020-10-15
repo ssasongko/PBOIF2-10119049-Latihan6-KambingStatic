1b7ec27c2e2aeb271e83ddfdd9c90eb65cf05766
Nur Sasongko <32429721+ssasongko@users.noreply.github.com>
2020-10-15 21:38:33 +0700

    Add files via upload

---
+++ PBOIF2-10119049-Latihan6-KambingStatic/src/pboif210119049latihan6kambingstatic/Mamalia.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif210119049latihan6kambingstatic;
+
+/**
+ *
+ * @author Nur Sasongko
+ */
+public class Mamalia {
+    public static int jumlahKambing;
+}
+
---
+++ PBOIF2-10119049-Latihan6-KambingStatic/src/pboif210119049latihan6kambingstatic/PBOIF210119049Latihan6KambingStatic.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif210119049latihan6kambingstatic;
+
+/**
+ * Nama  : Nur Sasongko
+ * Kelas : PBOIF2
+ * NIM   : 10119049
+ * Deskripsi Program : Program untuk menampilkan jumlah kambing
+ */
+
+
+public class PBOIF210119049Latihan6KambingStatic {
+
+    public static final String NAMA_KAMBING = "MIDUN";
+    
+    public static void main(String[] args) {
+        Mamalia.jumlahKambing = 485000;
+        System.out.println(NAMA_KAMBING + " memiliki kambing sebanyak " + Mamalia.jumlahKambing);
+    }
+    
+}
